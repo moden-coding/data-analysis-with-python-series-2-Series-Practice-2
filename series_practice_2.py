0ff2648ebb5874669de710dee9f4beaedca0676c
modengann <99417989+modengann@users.noreply.github.com>
2025-10-22 13:20:06 -0400

    Update series_practice_2.py

--- series_practice_2.py
+++ series_practice_2.py
@@ -1,14 +1,5 @@
 import pandas as pd
 
-dates = ["2023-11-01", "2023-11-02", "2023-11-03", "2023-11-04", "2023-11-05", "2023-11-06", "2023-11-07", "2023-11-08", "2023-11-09", "2023-11-10"]
-sales = [320, 400, 350, 380, 420, 360, 390, 410, 430, 410]
-
-#Create a series from the given data
-book_sales_series = pd.Series()
-
-
-
-
 def total_sales(sales_data):
     pass
 def date_with_lowest_sales(sales_data):
@@ -24,6 +15,12 @@ def days_with_sales_below(sales_data, threshold):
 
 def fancy_indexing_sales(sales_data, indices):
     pass
+
+dates = ["2023-11-01", "2023-11-02", "2023-11-03", "2023-11-04", "2023-11-05", "2023-11-06", "2023-11-07", "2023-11-08", "2023-11-09", "2023-11-10"]
+sales = [320, 400, 350, 380, 420, 360, 390, 410, 430, 410]
+
+#Create a series from the given data
+book_sales_series = pd.Series()
 
 
 
